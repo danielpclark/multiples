768b6c96c8bd6ff6821a7c0de040fcb215ed84ee
Daniel P. Clark <6ftdan@gmail.com>
2016-12-06 08:15:55 -0500

    Performance advantage of at least 2X

--- test/benches/multiples_benchmark.rb
+++ test/benches/multiples_benchmark.rb
@@ -2,6 +2,7 @@ require 'benchmark/ips'
 require 'multiples'
 
 premade = Multiples.new(3,5)
+premade2 = Multiples.new(7,11)
 
 Benchmark.ips do |x|
   x.report("Traditional") do
@@ -16,4 +17,15 @@ Benchmark.ips do |x|
     Multiples.new(3,5).lazy.select {|v| v % 7}.take(14).to_a
   end
 
+  x.report("Traditional2") do
+    (1..Float::INFINITY).lazy.select {|v| (v % 7 == 0 || v % 11 == 0) && v % 13 == 0 }.take(14).to_a
+  end
+
+  x.report("Multiples2") do
+    premade2.lazy.select {|v| v % 13}.take(14).to_a
+  end
+
+  x.report("new()-and-Multiples2") do
+    Multiples.new(7,11).lazy.select {|v| v % 13}.take(14).to_a
+  end
 end
